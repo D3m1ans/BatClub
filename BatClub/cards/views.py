30996956353897b336dfe9d1b43fbec49ef2dfa6
ne1ro <jack.20033505@gmail.com>
2024-09-12 16:25:38 +0300

    almost done card

--- BatClub/cards/views.py
+++ BatClub/cards/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render, get_object_or_404
 from .models import Card
 
@@ -6,4 +7,31 @@ from .models import Card
 
 def card_batman(request):
     card = get_object_or_404(Card, id=2)
-    return render(request, 'cards/card_batman.html', {'card': card})
+
+    appearance = card.appearance
+    parts = appearance.split('Bruce Wayne:')
+    hero_part = parts[0].strip()
+    person_part = parts[1].strip()
+
+    skills_text = card.skills
+    skills = re.split(r'\n\*', skills_text)
+    skills = [skill.strip() for skill in skills if skill.strip()]
+
+    allies_text = card.alliess
+    allies = re.split(r'\n\*', allies_text)
+    allies = [allie.strip() for allie in allies if allie.strip()]
+
+    enemies_text = card.enemises
+    enemies = re.split(r'\n\*', enemies_text)
+    enemies = [enemy.strip() for enemy in enemies if enemy.strip()]
+
+    context = {
+        'card': card,
+        'hero_part': hero_part,
+        'person_part': person_part,
+        'skills': skills,
+        'allies': allies,
+        'enemies': enemies,
+    }
+
+    return render(request, 'cards/card_batman.html', context)
