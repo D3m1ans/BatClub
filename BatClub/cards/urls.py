cc43b6ebf31622a846cb8fb9423830b8c6871d43
ne1ro <jack.20033505@gmail.com>
2024-09-12 19:52:03 +0300

    done batcard

--- BatClub/cards/urls.py
+++ BatClub/cards/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('batman', views.card_batman, name='batman-card'),
+    path('nightwing', views.card_nightwing, name='nightwing-card'),
 ]
--- BatClub/cards/views.py
+++ BatClub/cards/views.py
@@ -35,3 +35,34 @@ def card_batman(request):
     }
 
     return render(request, 'cards/card_batman.html', context)
+
+def card_nightwing(request):
+    card = get_object_or_404(Card, id=3)
+
+    appearance = card.appearance
+    parts = appearance.split('Dick Grayson')
+    hero_part = parts[0].strip()
+    person_part = parts[1].strip()
+
+    skills_text = card.skills
+    skills = re.split(r'\n\*', skills_text)
+    skills = [skill.strip() for skill in skills if skill.strip()]
+
+    allies_text = card.alliess
+    allies = re.split(r'\n\*', allies_text)
+    allies = [allie.strip() for allie in allies if allie.strip()]
+
+    enemies_text = card.enemises
+    enemies = re.split(r'\n\*', enemies_text)
+    enemies = [enemy.strip() for enemy in enemies if enemy.strip()]
+
+    context = {
+        'card': card,
+        'hero_part': hero_part,
+        'person_part': person_part,
+        'skills': skills,
+        'allies': allies,
+        'enemies': enemies,
+    }
+
+    return render(request, 'cards/card_nightwing.html', context)
