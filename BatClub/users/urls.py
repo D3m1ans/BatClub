b67fe5cb255a026d35fa4e9862d766c4004a0fab
ne1ro <jack.20033505@gmail.com>
2024-10-12 21:19:26 +0300

    password_reset (begin)

--- BatClub/users/urls.py
+++ BatClub/users/urls.py
@@ -5,5 +5,6 @@ from .views import RegisrerView
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register', RegisrerView.as_view(), name='register')
+    path('register', RegisrerView.as_view(), name='register'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset')
 ]
